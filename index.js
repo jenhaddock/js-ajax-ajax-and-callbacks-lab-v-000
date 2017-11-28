c895c934bf02480940d869e32b6a0f715232f2b2
Jennifer Haddock <jentx123@hotmail.com>
2017-11-28 03:31:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ $(document).ready(function (){
 function displayError(){
   $('#errors').html("I'm sorry, there's been an error. Please try again.");
 };
+
+function searchRepositories(){
+  const searchTerms = document.getElementById('searchTerms').value
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response){
+    $('#results').html(displayRepositories(response))
+  }).fail(displayError())
+};
