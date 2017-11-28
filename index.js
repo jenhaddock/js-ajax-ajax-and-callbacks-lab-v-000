1a3fbced23c172ee5c673f816fe2438be26ab0f6
Jennifer Haddock <jentx123@hotmail.com>
2017-11-28 03:34:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function searchRepositories(){
     $('#results').html(displayRepositories(response))
   }).fail(displayError())
 };
+
+function displayRepositories(response){
+  return response.repos.map(repo =>
+    `<div>
+        <h2><a href="${repo.html_url}">${repo.name}</a></h2>
+        <p>${repo.description}</p>
+        <img src="${repo.owner.avatar_url}" alt="${repo.owner.login}">
+        <a href="${repo.owner.html_url}">${repo.owner.login}</a>
+        <p><a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+      </div>`)
+};
